7ae564532d9f3e968845d814f8c5f014c3d7f37c
Hunter Seitz <59938540+hunter2seitz@users.noreply.github.com>
2023-06-30 08:19:40 -0400

    Update batch_watermark.py

--- batch_watermark.py
+++ batch_watermark.py
@@ -1,14 +1,12 @@
 import os
-from PIL import Image, ImageDraw
+from PIL import Image
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter.font import Font
-
 
 class WatermarkApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Simple Batch Image Watermarking")
+        self.title("Batch Image Watermarking")
         self.geometry("500x300")
 
         # Center the window on the screen
@@ -19,10 +17,10 @@ class WatermarkApp(tk.Tk):
         x = (screen_width - window_width) // 2
         y = (screen_height - window_height) // 2
         self.geometry(f"{window_width}x{window_height}+{x}+{y}")
-        font = Font(family="Arial", size=12)
 
         self.directory = ""
         self.watermark_path = ""
+        self.quality = 75
 
         self.directory_var = tk.StringVar()
         self.directory_var.set("No directory selected")
@@ -53,8 +51,19 @@ class WatermarkApp(tk.Tk):
         watermark_button = tk.Button(watermark_frame, text="Browse", command=self.select_watermark)
         watermark_button.pack(side=tk.LEFT, padx=10)
 
+        # Quality Selection
+        quality_frame = tk.Frame(self)
+        quality_frame.pack(pady=10)
+
+        quality_label = tk.Label(quality_frame, text="Compression Quality (1-95):")
+        quality_label.pack(side=tk.LEFT)
+
+        quality_entry = tk.Entry(quality_frame, width=10)
+        quality_entry.pack(side=tk.LEFT)
+        quality_entry.insert(tk.END, str(self.quality))
+        
         # Apply Watermark Button
-        apply_button = tk.Button(self, text="Apply Watermark", command=self.apply_watermark)
+        apply_button = tk.Button(self, text="Apply Watermark", command=lambda: self.apply_watermark(quality_entry.get()))
         apply_button.pack(pady=10)
 
         # Cancel Button
@@ -71,7 +80,7 @@ class WatermarkApp(tk.Tk):
         if self.watermark_path:
             self.watermark_var.set(self.watermark_path)
 
-    def apply_watermark(self):
+    def apply_watermark(self, quality):
         if not self.directory:
             messagebox.showerror("Error", "Please select a directory!")
             return
@@ -80,24 +89,28 @@ class WatermarkApp(tk.Tk):
             messagebox.showerror("Error", "Please select a watermark image!")
             return
 
+        try:
+            self.quality = int(quality)
+            if self.quality < 1 or self.quality > 95:
+                raise ValueError
+        except ValueError:
+            messagebox.showerror("Error", "Invalid compression quality!")
+            return
+
         watermark = Image.open(self.watermark_path).convert("RGBA")
 
         for filename in os.listdir(self.directory):
             if filename.lower().endswith(".jpg"):
                 image_path = os.path.join(self.directory, filename)
+                with Image.open(image_path).convert("RGB") as image:
+                    watermarked_image = self.apply_watermark_to_image(image, watermark)
+                    watermarked_image.save(image_path, "JPEG", quality=self.quality)
 
-                try:
-                    image = Image.open(image_path).convert("RGBA")
-                    watermarked_image = self.add_watermark(image, watermark)
-                    watermarked_image.save(image_path.replace(".jpg", "_watermarked.png"), "PNG", quality=10)
-                except (OSError, IOError, Image.UnidentifiedImageError):
-                    messagebox.showerror("Error", "Failed to apply watermark to image: {}".format(filename))
+        messagebox.showinfo("Success", "Watermark applied to images!")
 
-        messagebox.showinfo("Success", "Watermark applied to all images!")
-
-    def add_watermark(self, image, watermark):
-        watermark_width, watermark_height = watermark.size
+    def apply_watermark_to_image(self, image, watermark):
         image_width, image_height = image.size
+        watermark_width, watermark_height = watermark.size
 
         # Calculate the position to center the watermark
         x = (image_width - watermark_width) // 2
